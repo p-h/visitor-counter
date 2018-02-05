2306cc4b24877306b771ea7696235220b3e8ec44
Philippe Hürlimann <p@hurlimann.org>
2018-02-05 23:01:17 +0100

    Save and display per host hit counts aswell

--- src/Main.hs
+++ src/Main.hs
@@ -13,15 +13,22 @@ import Web.Scotty
 import qualified Text.Blaze.Html5 as H
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 
+import qualified Data.ByteString.Char8 as BS
+
 redisConnectInfo :: ConnectInfo
 redisConnectInfo = defaultConnectInfo { connectHost = "redis" }
 
-getAndIncreaseHits :: IO Integer
-getAndIncreaseHits = do
+getAndIncreaseHits :: String -> IO (Integer, Integer)
+getAndIncreaseHits hostName = do
     conn <- checkedConnect redisConnectInfo
     runRedis conn $ do
-        hitCount <- incr "hitCount"
-        return $ either (\_ -> 0) id hitCount
+        result <- multiExec $ do
+            hitCount' <- incr "hitCount"
+            hostHitCount' <- incr $ mconcat [BS.pack hostName, "_hitCount"]
+            return $ (,) <$> hitCount' <*> hostHitCount'
+        return $ case result of
+            TxSuccess tuple -> tuple
+            _ -> (0, 0)
 
 main :: IO ()
 main = do
@@ -29,15 +36,16 @@ main = do
     hostName <- getHostName
     scotty port $
         get "/" $ do
-            hits <- liftIO $ show <$> getAndIncreaseHits
+            (hits, hostHits) <- liftIO $ getAndIncreaseHits hostName
             html $ renderHtml
                 $ H.docTypeHtml $ do
                     H.head $ do
-                        H.title $ mapM_ H.toHtml ["Hello Visitor #", hits, "!"]
+                        H.title $ mapM_ H.toHtml ["Hello Visitor #", show hits, "!"]
                     H.body $ do
                         H.h1 "Hi there"
-                        H.p $ mapM_ H.toHtml ["You are visitor number: ", hits, "!"]
                         H.p $ mapM_ H.toHtml ["My name is: ", hostName]
+                        H.p $ mapM_ H.toHtml ["You are total visitor number: ", show hits, "!"]
+                        H.p $ mapM_ H.toHtml ["You are ", hostName, "'s visitor number: ", show hostHits, "!"]
 
 
 
